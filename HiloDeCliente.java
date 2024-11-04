a4352d52278262dde0034bbcce6093854acf4c31
Rodrigo Dominguez Araya <rdominguez19@alumnos.utalca.cl>
2024-11-04 02:41:27 -0300

    historial de mensajes

--- HiloDeCliente.java
+++ HiloDeCliente.java
@@ -1,6 +1,11 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
 
@@ -15,7 +20,7 @@ public class HiloDeCliente implements Runnable {
     private DataOutputStream dataOutput;
     private Usuario usuario;
 
-    public HiloDeCliente(Socket socket,  Usuario usuario) {
+    public HiloDeCliente(Socket socket, Usuario usuario) {
         try {
             this.usuario = usuario;
             System.out.println("Usuario: " + usuario);
@@ -23,7 +28,7 @@ public class HiloDeCliente implements Runnable {
             dataInput = new DataInputStream(socket.getInputStream());
             dataOutput = new DataOutputStream(socket.getOutputStream());
             clientes.add(this);
-
+            cargarMensajes(); // Cargar mensajes anteriores
             enviarListaUsuarios();
         } catch (Exception e) {
             e.printStackTrace();
@@ -44,33 +49,51 @@ public class HiloDeCliente implements Runnable {
 
                     for (HiloDeCliente cliente : clientes) {
                         if (cliente.correoUsuario().equals(destinatario)) {
+                            String mensajeParaGuardar = correoUsuario() + ": " + mensajePrivado;
+                            guardarMensaje("[Privado de " + correoUsuario() + "]: " + mensajeParaGuardar);
                             cliente.dataOutput.writeUTF("[Privado de " + correoUsuario() + "]: " + mensajePrivado);
                             break;
                         }
                     }
-                }
-                else if(mensaje.startsWith("/")) {
+                } else if (mensaje.startsWith("/")) {
                     String[] partes = mensaje.split(":", 2);
                     String destinatario = partes[0].substring(1); // Obtener destinatario
                     String mensajeGrupo = partes[1];
 
                     for (HiloDeCliente cliente : clientes) {
-                        System.out.println("Cliente clase: "+cliente.usuario.getClass().getSimpleName());
-                        System.out.println("Destinatario: "+destinatario);
+                        System.out.println("Cliente clase: " + cliente.usuario.getClass().getSimpleName());
+                        System.out.println("Destinatario: " + destinatario);
                         if (cliente.usuario.getClass().getSimpleName().equals(destinatario)) {
-                            cliente.dataOutput.writeUTF("[ "+ correoUsuario() +" para grupo de " + destinatario +"]: " + mensajeGrupo);
+                            String mensajeParaGuardar = correoUsuario() + " para grupo de " + destinatario + ": "
+                                    + mensajeGrupo;
+                            guardarMensaje("[ " + correoUsuario() + " para grupo de " + destinatario + "]: "
+                                    + mensajeParaGuardar);
+                            cliente.dataOutput.writeUTF(
+                                    "[ " + correoUsuario() + " para grupo de " + destinatario + "]: " + mensajeGrupo);
                         }
                     }
-                }
-                else {
+                } else {
                     // Enviar mensaje general a todos
                     for (HiloDeCliente cliente : clientes) {
+                        String mensajeParaGuardar = correoUsuario() + ": " + mensaje;
+                        System.out.println("Guardando mensaje: " + mensajeParaGuardar);
+                        guardarMensaje(mensajeParaGuardar); // Guardar en archivo
                         cliente.dataOutput.writeUTF(correoUsuario() + ": " + mensaje);
                     }
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            // manejo de desconexiones
+            try {
+                clientes.remove(this);
+                enviarListaUsuarios();
+                dataInput.close();
+                dataOutput.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -78,7 +101,8 @@ public class HiloDeCliente implements Runnable {
     private void enviarListaUsuarios() {
         StringBuilder listaUsuarios = new StringBuilder("#usuarios:");
         for (HiloDeCliente cliente : clientes) {
-            listaUsuarios.append(cliente.correoUsuario()).append(",").append(cliente.usuario.getClass().getSimpleName()).append(";"); // Añadir el usuario a la lista
+            listaUsuarios.append(cliente.correoUsuario()).append(",").append(cliente.usuario.getClass().getSimpleName())
+                    .append(";"); // Añadir el usuario a la lista
         }
         // Eliminar la última coma
         if (listaUsuarios.length() > 0) {
@@ -96,5 +120,45 @@ public class HiloDeCliente implements Runnable {
 
     public String correoUsuario() {
         return usuario.getCorreo();
+    }
+
+    private void guardarMensaje(String mensaje) {
+        // Lógica para guardar el mensaje al fichero
+        try (FileWriter fw = new FileWriter("historial.txt", true);
+                BufferedWriter bw = new BufferedWriter(fw);
+                PrintWriter out = new PrintWriter(bw)) {
+            // Leer el último mensaje guardado
+            String ultimoMensaje = "";
+            try (BufferedReader br = new BufferedReader(new FileReader("historial.txt"))) {
+                String linea;
+                while ((linea = br.readLine()) != null) {
+                    ultimoMensaje = linea;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // Verificar si el mensaje es igual al último mensaje guardado
+            if (ultimoMensaje.equals(mensaje)) {
+                return;
+            } else {
+                out.println(mensaje);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void cargarMensajes() {
+        try (BufferedReader br = new BufferedReader(new FileReader("historial.txt"))) {
+            String line;
+            // Enviar el historial solo al cliente que se está reconectando
+            while ((line = br.readLine()) != null) {
+                // solo enviar mensaje si no ha sido enviado previamente
+                dataOutput.writeUTF(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
